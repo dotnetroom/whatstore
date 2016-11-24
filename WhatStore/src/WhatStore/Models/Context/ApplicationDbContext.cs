2a3dd76e0dc63a4e4647867e1d7daed7f8ae36ee
LuizGustavoPereira <pereiraguus@gmail.com>
2016-11-24 17:15:00 -0200

    DbSet

--- WhatStore/src/WhatStore/Models/Context/ApplicationDbContext.cs
+++ WhatStore/src/WhatStore/Models/Context/ApplicationDbContext.cs
@@ -5,16 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WhatStore.Crosscutting.Infrastructure.Models.Identity;
+using WhatStore.Crosscutting.Infrastructure.Models.Product;
 
 namespace WhatStore.Models.Context
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>
     {
+        
+
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
         }
+
+        public DbSet<PictureProduct> PictureProduct { get; set; }
+        public DbSet<Product.Product> Product { get; set; }
+
+
     }
 }
