1c7a79087a62799139d6523eab8c2660f21d2c63
LuizGustavoPereira <pereiraguus@gmail.com>
2016-11-24 17:36:18 -0200

    ProductSize

--- WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Product/ProductSize.cs
+++ WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Product/ProductSize.cs
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-﻿using System.ComponentModel.DataAnnotations;
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
->>>>>>> origin/1.0
 
 namespace WhatStore.Crosscutting.Infrastructure.Models.Product
 {
@@ -16,7 +12,6 @@ namespace WhatStore.Crosscutting.Infrastructure.Models.Product
         public string ProductId { get; set; }
         [Required]
         public int SizeId { get; set; }
-
         public virtual Product Product { get; set;}
         public virtual Size Size  { get; set; }
     }
