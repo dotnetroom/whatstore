7857ca9ae7eeb955853e4a85861220d00262ac4e
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2016-11-24 16:22:32 -0200

    class

--- WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Store/Adress.cs
+++ WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Store/Adress.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,18 @@ namespace WhatStore.Crosscutting.Infrastructure.Models.Store
 {
     public class Adress
     {
+        [Key]
+        [Required]
         public long ID { get; set; }
+        [Required]
         public int CityID { get; set; }
+        [Required]
         public String CEP { get; set; }
+        [Required]
         public String Number { get; set; }
+        [Required]
         public String Street { get; set; }
+        [Required]
         public String Complement { get; set; }
 
         public virtual City City { get; set; }
