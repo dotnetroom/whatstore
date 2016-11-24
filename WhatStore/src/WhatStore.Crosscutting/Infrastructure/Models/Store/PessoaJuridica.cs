8e0e6e5f24599facb9bd2fd1e079bbacd1efb78d
lamnaldi <laramenaldi@gmail.com>
2016-11-24 16:19:28 -0200

    Add class Pessoa Juridica

---
+++ WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Store/PessoaJuridica.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Crosscutting.Infrastructure.Models.Store
+{
+    public class PessoaJuridica
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string CNPJ { get; set; }
+        [Required]
+        public string InscricaoEstadual { get; set; }
+        [Required]
+        public string InscricaoMunicipal { get; set; }
+        [Required]
+        public string RazaoSocial { get; set; }
+    }
+}
