b15ccd57cd8d351b289f8ace27a922661bf339a0
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2016-11-24 15:41:31 -0200

    Classe
    Inserção das classes city e state

---
+++ WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Store/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Crosscutting.Infrastructure.Models.Store
+{
+    public class City
+    {
+        public int CityID { get; set; }
+        public int StateID { get; set; }
+        public String Name { get; set; }
+
+        public virtual State State { get; set; }
+    }
+}
---
+++ WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Store/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Crosscutting.Infrastructure.Models.Store
+{
+    public class State
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+    }
+}
