a157a650e09148aee1cf56343ff516b5ac93201b
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2016-11-24 16:28:15 -0200

    Class Store

--- WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Store/Store.cs
+++ WhatStore/src/WhatStore.Crosscutting/Infrastructure/Models/Store/Store.cs
@@ -11,17 +11,16 @@ namespace WhatStore.Crosscutting.Infrastructure.Models.Store
         [Key]
         [Required]
         public long ID { get; set; }
-
         [Required]
         public long PessoaJuridicaID { get; set; }
         [Required]
         public long AdressID { get; set; }
-
+        [Required]
+        public int StoreTypeID { get; set; }
         [Required]
         public String Name { get; set; }
         [Required]
         public String Description { get; set; }
-        [Required]
         public String Phone { get; set; }
         [Required]
         public String Email { get; set; }
@@ -35,5 +34,7 @@ namespace WhatStore.Crosscutting.Infrastructure.Models.Store
         public String Term { get; set; }
 
         public virtual Adress Adress { get; set; }
+        public virtual StoreType StoreType { get; set; }
+        public virtual PessoaJuridica PessoaJuridica { get; set; }
     }
 }
