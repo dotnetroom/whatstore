b1358a049d9f323ac0e7235091527bc651646b49
lamnaldi <laramenaldi@gmail.com>
2016-12-06 17:35:30 -0200

    Product

---
+++ WhatStore/src/WhatStore/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WhatStore.Infrastructure.ViewModels.Admin;
+
+namespace WhatStore.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterProduct(RegisterProductViewModel Product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+                return Ok();
+            
+
+        }
+    }
+}
