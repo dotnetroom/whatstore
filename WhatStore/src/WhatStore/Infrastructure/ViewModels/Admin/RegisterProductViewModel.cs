eb69a74e389d21d870b59a6ab59963a46434dad0
lamnaldi <laramenaldi@gmail.com>
2016-12-06 13:52:32 -0200

    RegisterProductViewModel

--- WhatStore/src/WhatStore/Infrastructure/ViewModels/Admin/RegisterProductViewModel.cs
+++ WhatStore/src/WhatStore/Infrastructure/ViewModels/Admin/RegisterProductViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WhatStore.Crosscutting.Infrastructure.Models.Product;
 
 namespace WhatStore.Infrastructure.ViewModels.Admin
 {
@@ -10,5 +12,12 @@ namespace WhatStore.Infrastructure.ViewModels.Admin
         public string NameProduct { get; set; }
         public string Description { get; set; }
         public string Price { get; set; }
+        public string Color { get; set; }
+        public List<ProductSize> Size { get; set; }
+        public string Weight { get; set; }
+        public string Height { get; set; }
+        public string Length { get; set; }
+        public ICollection<IFormFile> Picture { get; set; }
+        public List<Tag> Tags { get; set; }
     }
 }
---
+++ WhatStore/src/WhatStore/Infrastructure/ViewModels/Admin/RegisterUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Infrastructure.ViewModels.Admin
+{
+    public class RegisterUserViewModel
+    {
+    }
+}
