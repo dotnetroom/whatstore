322b599b00b17ecf7447c63e644a6cc5e48dd1a9
lamnaldi <laramenaldi@gmail.com>
2016-12-06 14:03:03 -0200

    RegisterUserViewModel

--- WhatStore/src/WhatStore/Infrastructure/ViewModels/Admin/RegisterUserViewModel.cs
+++ WhatStore/src/WhatStore/Infrastructure/ViewModels/Admin/RegisterUserViewModel.cs
@@ -7,5 +7,13 @@ namespace WhatStore.Infrastructure.ViewModels.Admin
 {
     public class RegisterUserViewModel
     {
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool Genero { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
