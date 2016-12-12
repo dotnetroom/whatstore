5ce1a81c7bfb84332565c769ebfb6e2ea421350f
LuizGustavoPereira <pereiraguus@gmail.com>
2016-12-12 17:08:06 -0200

    oioi

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/Repository/StoreTypeRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Store;
+using WhatStore.Domain.Infrastructure.Repository.Interfaces;
+
+namespace WhatStore.Crosscutting.Infrastructure.Repository
+{
+    public class StoreTypeRepository : IStoreTypeRepository
+    {
+        public Task<List<StoreType>> GetTypes()
+        {
+            try
+            {
+                
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
