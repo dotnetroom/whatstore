f7ac883de25b8d0fa32e531f9e9900cc6221cdca
LuizGustavoPereira <pereiraguus@gmail.com>
2016-12-12 17:10:02 -0200

    o

---
+++ WhatStore/src/WhatStore/Infrastructure/ViewModels/Store/StoreTypeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Store;
+
+namespace WhatStore.Infrastructure.ViewModels.Admin
+{
+    public class StoreTypeViewModel
+    {
+        public List<StoreType> Types { get; set; }
+    }
+}
