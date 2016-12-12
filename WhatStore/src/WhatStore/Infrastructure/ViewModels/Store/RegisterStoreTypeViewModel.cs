344a6bcfae3dd98dddf5ce366912d90e3392cbf7
LuizGustavoPereira <pereiraguus@gmail.com>
2016-12-12 17:09:14 -0200

    oi dnovo

---
+++ WhatStore/src/WhatStore/Infrastructure/ViewModels/Store/RegisterStoreTypeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Infrastructure.ViewModels.Store
+{
+    public class RegisterStoreTypeViewModel
+    {
+
+        public string StoreType { get; set; } 
+    }
+}
