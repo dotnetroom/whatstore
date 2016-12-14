9d15f708f465ed7eea85ae73af8d150ceeaca1c1
LuizGustavoPereira <pereiraguus@gmail.com>
2016-12-14 16:33:10 -0200

    Login

--- WhatStore/src/WhatStore/Controllers/AccountController.cs
+++ WhatStore/src/WhatStore/Controllers/AccountController.cs
@@ -7,6 +7,9 @@ using WhatStore.Infrastructure.ViewModels.Admin;
 using Microsoft.AspNetCore.Identity;
 using WhatStore.Domain.Infrastructure.Repository.Interfaces;
 using WhatStore.Domain.Infrastructure.Models.Store;
+using Microsoft.AspNetCore.Authorization;
+using WhatStore.Domain.Infrastructure.Models.Identity;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,20 +20,28 @@ namespace WhatStore.Controllers
     {
 
         private IStoreRepository _storeRepository;
-        public AccountController(IStoreRepository storeRepository)
+        private SignInManager<ApplicationUser> _signInManager;
+        private ILogger _logger;
+
+        public AccountController(IStoreRepository storeRepository, SignInManager<ApplicationUser> signInManager, ILogger logger)
         {
             _storeRepository = storeRepository;
+            _signInManager = signInManager;
+            _logger = logger;
         }
 
         // GET: /<controller>/
 
         [Route("login")]
-        public IActionResult Login()
+        [AllowAnonymous]
+        public IActionResult Login(string returnURL = null)
         {
+            ViewData["ReturnURL"] = returnURL;
             return View();
         }
 
         [Route("register")]
+        [AllowAnonymous]
         public async Task<IActionResult> Register()
         {
             var storeTypes = await _storeRepository.GetStoreType();
@@ -45,12 +56,17 @@ namespace WhatStore.Controllers
 
         // POST: /<controller>/
         [HttpPost("login")]
-        public async Task<IActionResult> Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model, string returnURL = null)
         {
+            ViewData["ReturnURL"] = returnURL;
+
             if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe ,lockoutOnFailure: false);
+
             return Ok();
         }
 
--- WhatStore/src/WhatStore/Infrastructure/ViewModels/Account/LoginViewModel.cs
+++ WhatStore/src/WhatStore/Infrastructure/ViewModels/Account/LoginViewModel.cs
@@ -9,9 +9,14 @@ namespace WhatStore.Infrastructure.ViewModels.Admin
     public class LoginViewModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
     }
 }
