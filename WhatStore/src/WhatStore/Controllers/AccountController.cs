d9c53cdb8b6eab3d037cac37788fc8c3c64b5daf
lamnaldi <laramenaldi@gmail.com>
2016-12-14 14:07:53 -0200

    AccountControler

--- WhatStore/src/WhatStore/Controllers/AccountController.cs
+++ WhatStore/src/WhatStore/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WhatStore.Infrastructure.ViewModels.Admin;
 using Microsoft.AspNetCore.Identity;
+using WhatStore.Domain.Infrastructure.Models.Identity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,6 +14,12 @@ namespace WhatStore.Controllers
     [Route("account")]
     public class AccountController : Controller
     {
+        private UserManager<ApplicationUser> _userManager;
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         // GET: /<controller>/
 
         [Route("login")]
@@ -38,14 +45,23 @@ namespace WhatStore.Controllers
             return Ok();
         }
 
-        /*[HttpPost("register")]
-        public async Task<IActionResult> Register(LoginViewModel model)
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
+
+            var storeID = _store.RegisterStore();
+
+            var newUser = new ApplicationUser()
+            {
+                StoreId = storeID
+            };
+            _userManager.CreateAsync(_userManager);
+
             return Ok();
-        }*/
+        }
     }
 }
