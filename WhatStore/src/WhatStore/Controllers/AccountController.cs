1761a30996d93a89fe42e1bf339bfe4ec99ecae5
LuizGustavoPereira <pereiraguus@gmail.com>
2016-12-14 15:25:25 -0200

    AccountController

--- WhatStore/src/WhatStore/Controllers/AccountController.cs
+++ WhatStore/src/WhatStore/Controllers/AccountController.cs
@@ -5,12 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WhatStore.Infrastructure.ViewModels.Admin;
 using Microsoft.AspNetCore.Identity;
-<<<<<<< HEAD
 using WhatStore.Domain.Infrastructure.Repository.Interfaces;
 using WhatStore.Domain.Infrastructure.Models.Store;
-=======
-using WhatStore.Domain.Infrastructure.Models.Identity;
->>>>>>> origin/1.0
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,18 +15,11 @@ namespace WhatStore.Controllers
     [Route("account")]
     public class AccountController : Controller
     {
-<<<<<<< HEAD
 
         private IStoreRepository _storeRepository;
         public AccountController(IStoreRepository storeRepository)
         {
             _storeRepository = storeRepository;
-=======
-        private UserManager<ApplicationUser> _userManager;
-        public AccountController(UserManager<ApplicationUser> userManager)
-        {
-            _userManager = userManager;
->>>>>>> origin/1.0
         }
 
         // GET: /<controller>/
@@ -56,7 +45,7 @@ namespace WhatStore.Controllers
 
         // POST: /<controller>/
         [HttpPost("login")]
-        public async Task<IActionResult> Login (LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (!ModelState.IsValid)
             {
@@ -73,34 +62,23 @@ namespace WhatStore.Controllers
                 return BadRequest();
             }
 
-<<<<<<< HEAD
-            var storeID = await _storeRepository.RegisterStore(new Store() {
+            var storeID = await _storeRepository.RegisterStore(new Store()
+            {
                 StoreTypeId = model.StoreTypeID,
                 Name = model.StoreName.Trim(),
                 URL = model.StoreName.Trim().Replace(" ", "").ToLower()
             });
 
             if (storeID > 0)
-             {
-                 model.ReturnMessage = "Alterações salvas com sucesso";
-             }
-             else
-             {
-                 model.ReturnMessage = "Erro ao salvar alterações";
-             }
+            {
+                model.ReturnMessage = "Alterações salvas com sucesso";
+            }
+            else
+            {
+                model.ReturnMessage = "Erro ao salvar alterações";
+            }
 
             return View("Login", model);
-=======
-            var storeID = _store.RegisterStore();
-
-            var newUser = new ApplicationUser()
-            {
-                StoreId = storeID
-            };
-            _userManager.CreateAsync(_userManager);
-
-            return Ok();
->>>>>>> origin/1.0
         }
     }
 }
