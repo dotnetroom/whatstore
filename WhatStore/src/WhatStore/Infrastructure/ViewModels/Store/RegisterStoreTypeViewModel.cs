8ede4d30938db77ae93ce7b6559f77915ad5fcc4
LuizGustavoPereira <pereiraguus@gmail.com>
2016-12-16 17:06:04 -0200

    StoreTypes

--- WhatStore/src/WhatStore/Infrastructure/ViewModels/Store/RegisterStoreTypeViewModel.cs
+++ WhatStore/src/WhatStore/Infrastructure/ViewModels/Store/RegisterStoreTypeViewModel.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Store;
 
 namespace WhatStore.Infrastructure.ViewModels.Store
 {
     public class RegisterStoreTypeViewModel
     {
+        public List<StoreType> StoreTypes { get; set; }
 
         public string StoreType { get; set; }
 
