19e01e65f1f081e0dc1d4d445ba3883e80cd11ea
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2016-12-16 15:19:29 -0200

    Admin View Model

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/FinancialViewMode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Admin
+{
+    public class FinancialViewMode
+    {
+    }
+}
