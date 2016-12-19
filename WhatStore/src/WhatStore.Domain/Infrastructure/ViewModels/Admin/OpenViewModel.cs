cb67602e301bbddcdbef5b145973b059e6661e3e
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2016-12-19 16:49:33 -0200

    ViewModels

--- WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/OpenViewModel.cs
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/OpenViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WhatStore.Infrastructure.ViewModels.Admin
+namespace WhatStore.Domain.Infrastructure.ViewModels.Admin
 {
     public class OpenViewModel
     {
