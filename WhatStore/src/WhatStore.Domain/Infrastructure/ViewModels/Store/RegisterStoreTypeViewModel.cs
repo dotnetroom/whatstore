f9153d2598b4b6e0ab86fcea4f9453b8820942d0
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2016-12-19 17:03:03 -0200

    oi

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Store/RegisterStoreTypeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Store;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Store
+{
+    public class RegisterStoreTypeViewModel
+    {
+
+        public List<StoreType> StoreTypes { get; set; }
+
+        public string StoreType { get; set; }
+
+        public string ReturnMessage { get; set; }
+
+    }
+}
