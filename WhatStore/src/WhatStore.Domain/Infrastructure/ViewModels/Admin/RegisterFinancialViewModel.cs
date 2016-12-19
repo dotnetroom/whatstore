70d1572826a718638d9a74a0714a2b6620db0cbb
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2016-12-19 16:49:15 -0200

    RegisterFinancialViewModel

--- WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/RegisterFinancialViewModel.cs
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/RegisterFinancialViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WhatStore.Infrastructure
+namespace WhatStore.Domain.Infrastructure
 {
     public class RegisterFinancialViewModel
     {
