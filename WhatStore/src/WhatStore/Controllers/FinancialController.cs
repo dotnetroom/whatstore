6e19e09d0314eab4d15beacaa45916e704b674b4
LuizGustavoPereira <pereiraguus@gmail.com>
2016-12-20 15:42:06 -0200

    FinancialController

--- WhatStore/src/WhatStore/Controllers/FinancialController.cs
+++ WhatStore/src/WhatStore/Controllers/FinancialController.cs
@@ -1,10 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WhatStore.Domain.Infrastructure;
-<<<<<<< HEAD
-
-=======
->>>>>>> a5faa99007dbe40310e897918c14b9dad2f1a07d
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
