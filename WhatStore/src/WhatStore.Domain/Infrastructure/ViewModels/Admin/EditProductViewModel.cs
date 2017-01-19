3effd2593994770ae23b2dd4c05d3899de1f338c
LuizGustavoPereira <pereiraguus@gmail.com>
2017-01-19 13:46:19 -0200

    .

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/EditProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Product;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Admin
+{
+    public class EditProductViewModel
+    {
+        public List<Product> Products { get; set; }
+    } 
+}
