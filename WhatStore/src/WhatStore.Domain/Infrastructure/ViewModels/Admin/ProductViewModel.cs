d85b9549d53327dbc9a3459dff7306d557cc4b32
LuizGustavoPereira <pereiraguus@gmail.com>
2017-01-20 13:10:05 -0200

    viewModel

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Product;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Admin
+{
+    public class ProductViewModel
+    {
+        public List<Product> Products { get; set; }
+        
+    } 
+}
