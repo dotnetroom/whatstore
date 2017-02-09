32b127766c4e1515a4967cddf03ad7b35dd748f9
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-02-09 13:39:31 -0200

    ...

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/PictureProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Admin
+{
+    public class PictureProductViewModel
+    {
+        public long Id { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string ProductId { get; set; }
+    }
+}
