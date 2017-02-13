e5e53f5d98124d6904683f162e43871a8de00d1c
lamnaldi <laramenaldi@gmail.com>
2017-02-13 15:32:12 -0200

    CategoryViewModel

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/RegisterProductCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Product;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Admin
+{
+    public class RegisterProductCategoryViewModel
+    {
+
+        public List<Category> Categorys { get; set; }
+        public string ProductCategory { get; set; }
+    }
+}
