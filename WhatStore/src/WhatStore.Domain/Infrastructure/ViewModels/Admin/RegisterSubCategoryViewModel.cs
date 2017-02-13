993c62797679071279092a3ce1b8597bc3e93aee
LuizGustavoPereira <pereiraguus@gmail.com>
2017-02-13 15:23:03 -0200

    RegisterSubCategoryViewModel

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Admin/RegisterSubCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Product;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Admin
+{
+    public class RegisterSubCategory
+    {
+        public List<SubCategory> dubCategorys { get; set; }
+        public string subCategorys { get; set; }
+    }
+}
