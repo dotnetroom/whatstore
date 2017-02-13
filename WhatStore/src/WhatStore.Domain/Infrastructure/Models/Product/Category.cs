de539409ca5c8eb80cbdf96a34d42b7d9ec4a7c4
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-02-13 13:21:21 -0200

    ....

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/Models/Product/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Domain.Infrastructure.Models.Product
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
