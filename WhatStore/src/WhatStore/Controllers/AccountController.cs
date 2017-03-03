59a1ba4b365e113dcf6581afffca3348d23e0c0f
lamnaldi <laramenaldi@gmail.com>
2017-03-03 15:09:06 -0300

    .

--- WhatStore/src/WhatStore/Controllers/AccountController.cs
+++ WhatStore/src/WhatStore/Controllers/AccountController.cs
@@ -199,9 +199,11 @@ namespace WhatStore.Controllers
         {
 
             var states = await _localizationRepository.GetStates();
+            var cities = await _localizationRepository.GetCities();
 
             var model = new RegisterUserStoreCompViewModel();
             model.States = states;
+            model.CityID = cities;
 
             return View(model);
         }
