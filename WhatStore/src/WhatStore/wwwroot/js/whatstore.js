9959e70d399b59a572973bc1050c149111308d75
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-03-06 14:47:39 -0300

    drop SubCategory

--- WhatStore/src/WhatStore/wwwroot/js/whatstore.js
+++ WhatStore/src/WhatStore/wwwroot/js/whatstore.js
@@ -24,4 +24,27 @@
         });
     };
 
+    $.fn.category = function (dropSubCategories) {
+        var dropDown = $(this);
+
+        dropDown.on('change', function () {
+            var categoryId = $(this).val();
+            dropSubCategories.html('<option value="-1">Selecione a subcategoria</option>');
+
+            $.ajax({
+                url: '/product/list/subcategory/' + categoryId,
+                method: 'GET',
+                success: function (data) {
+                    $.each(data, function (i, item) {
+                        dropSubCategories.append('<option value="' + item.id + '">' + item.subCategoryName + '<option>');
+                    });
+                    dropSubCategories.removeAttr('disabled');
+                },
+                error: function (x, y, message) {
+                    dropSubCategories.attr('disable', 'disable');
+                }
+            });
+        });
+    };
+
 })(jQuery);
