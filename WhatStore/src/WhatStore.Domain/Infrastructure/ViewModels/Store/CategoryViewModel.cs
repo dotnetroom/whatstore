97350ab77dff92bec8770f57b8e83f7ac3d9acd3
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-03-09 17:22:39 -0300

    categoryViewModel

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Store/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatStore.Domain.Infrastructure.Models.Product;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Store
+{
+    public class CategoryViewModel
+    {
+        public long Id { get; set; }
+        public Category category { get; set; }
+        public List<SubCategory> subcategories { get; set; }
+    }
+}
