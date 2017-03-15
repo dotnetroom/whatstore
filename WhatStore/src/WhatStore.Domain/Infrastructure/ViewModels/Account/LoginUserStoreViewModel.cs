8bcdf8083ed1a30c9acf20bdb137cb8523446fc6
lamnaldi <laramenaldi@gmail.com>
2017-03-15 17:19:49 -0300

    .

---
+++ WhatStore/src/WhatStore.Domain/Infrastructure/ViewModels/Account/LoginUserStoreViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatStore.Domain.Infrastructure.ViewModels.Account
+{
+    public class LoginUserStoreViewModel
+    {
+       
+        public string Email { get; set; }
+
+    
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        public string EmailCadastro { get; set; }
+
+    }
+}
